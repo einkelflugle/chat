ac03e730bb6d73e1b970f646b34b0f309caf5e1c
einkelflugle <cluckyferret@gmail.com>
2015-07-19 21:12:38 +1000

    Changes server.js to allow port 80 connections

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var connect = require('connect')
 var serveStatic = require('serve-static')
-connect().use(serveStatic(__dirname + "/client/build")).listen(8100)
-console.log('Started node.js server on http://localhost:8100')
+connect().use(serveStatic(__dirname + "/client/build")).listen(80, function(error) {
+	if (error) return cb(error)
+
+	// Remove root permissions
+	var uid = parseInt(process.env.SUDO_UID);
+	if (uid) process.setuid(uid);
+
+	console.log('Started node.js server on http://localhost:80')
+})
